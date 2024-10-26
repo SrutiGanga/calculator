8613eb5dfb0fa5fea8695f05f021fc64d82bfcc7
SrutiGanga <99947565+SrutiGanga@users.noreply.github.com>
2024-10-26 06:21:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const display =document.getElementById("display");
 
 function appendToDisplay(input){
-  display.value+=input;
   display.value-=input;
-  display.value*=input;
-  display.value/=input;
+ 
   
 }
 function clearDisplay(){
